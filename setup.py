6fce9b7bb9bcceb9f7a0b15bc14b5293e2713ba3
Luis Seabra <7190666+luismavs@users.noreply.github.com>
2022-04-11 15:39:47 +0100

    requirements versions (#4)

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_namespace_packages, setup
 
 setup(
     name="kitsunetorch",
-    version="0.2.2",
+    version="0.2.3",
     description="Kitsune anomaly detection model implemented in PyTorch.",
     author="Guillem Orellana Trullols",
     author_email="guillem.orellana@gmail.com",
@@ -11,13 +11,13 @@ setup(
     packages=find_namespace_packages(include=["kitsune"]),
     python_requires="~=3.8",
     install_requires=[
-        "typer~=0.3.2",
-        "torch~=1.11.0",
-        "torchdata~=0.3.0",
+        "typer",
+        "torch~=1.11",
+        "torchdata~=0.3",
         "pandas~=1.2",
         "scikit-learn~=0.24",
-        "scipy~=1.8.0",
-        "tqdm~=4.64",
+        "scipy~=1.8",
+        "tqdm",
     ],
     zip_safe=False,
 )
